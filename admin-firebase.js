cdc1c54c559233f1117b6bb5764f1fe8afa8c229
audrynel <munezeronadin@gmail.com>
2026-09-23 01:39:58 +0200

    Add files via upload

--- admin-firebase.js
+++ admin-firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import { getFirestore, collection, getDocs, getDoc, doc } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import {
+  getFirestore, collection, getDocs, getDoc, doc, setDoc, deleteDoc
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAPw-3ApKeXuLZM2TULUVKeuM4ZbL0nWGU",
@@ -11,6 +13,8 @@ const firebaseConfig = {
   appId: "1:159483195754:web:95e5dd56c326c0d4937513",
   measurementId: "G-BDNXVS3FHH"
 };
+
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export { collection, getDocs, getDoc, doc };
+const db = getFirestore(app);
+
+export { db, collection, getDocs, getDoc, doc, setDoc, deleteDoc };
